8fe97788e55a25123595aae31568deaf13ed2c0c
AironRuda <airon.ruda@udea.edu.co>
2022-07-27 17:21:25 -0500

    Cabecera

--- aprendiendoreact/src/App.js
+++ aprendiendoreact/src/App.js
@@ -1,11 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from "./assets/images/logo.svg";
+import "./assets/css/App.css";
 import MiComponente from "./components/MiComponente";
 import Peliculas from "./components/Peliculas";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+
+      <Header/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
