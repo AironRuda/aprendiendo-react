00c95580dc90b5d5f403daa8d72508604afe5886
AironRuda <airon.ruda@udea.edu.co>
2022-07-28 13:11:50 -0500

    Estados de los componentes

--- aprendiendoreact/src/components/SeccionPruebas.js
+++ aprendiendoreact/src/components/SeccionPruebas.js
@@ -4,7 +4,31 @@ import MiComponente from "./MiComponente";
 import Peliculas from "./Peliculas";
 
 class SeccionPruebas extends Component {
-  HolaMundo(nombre, edad) { // Se convierte en un metodo
+  /**
+   * Metodos se llaman con this.methosName
+   * funciones se llaman con functionName
+   */
+
+  // Propiedades de la clase
+  contador = 0;
+  /**
+   * Para trabajar con estados se debe crear un constructor
+   *
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contador: 0,
+    };
+  }
+  */
+ state = {
+    // Puede objetos o colecciones de objetos
+    contador: 0,
+ }
+
+  HolaMundo(nombre, edad) {
+    // Se convierte en un metodo
     var presentacion = (
       <div>
         <h2>Hola, soy {nombre}</h2>
@@ -14,17 +38,44 @@ class SeccionPruebas extends Component {
     return presentacion;
   }
 
+  sumar(e) {
+    //this.contador += 1;
+    /**
+     * Para que lea estimulos se deben setiar los estados
+     * 
+     * La funcion cunaod fue llamada debio ser bindeada con this .bind(this)
+     */
+    this.setState({
+      contador: this.state.contador + 1,
+    });
+  }
+  restar(e) {
+    this.setState({
+        contador: this.state.contador - 1,
+      });
+  }
   render() {
     var nombre = "Airon Ruda";
     return (
       <section id="content">
-        <h2 class="subHeader">Ultimos articulos</h2>
+        <h2 className="subHeader">Ultimos articulos</h2>
         <p>Hola bienvenido al aprendiendo react.</p>
+
+        <h2 className="subHeader">Funciones y JSX basico</h2>
         {this.HolaMundo(nombre, 12)}
+
+        <h2 className="subHeader">Componentes</h2>
         <section className="componentes">
           <MiComponente />
           <Peliculas />
         </section>
+
+        <h2 className="subHeader">Estado</h2>
+        <p>contador: {this.state.contador}</p>
+        <p>
+          <input type="button" value="sumar" onClick={this.sumar.bind(this)} />
+          <input type="button" value="restar" onClick={this.restar.bind(this)} />
+        </p>
       </section>
     );
   }
